270eee3f3d755fcedaa5a45f38fc48114987458b
Ákos <kokoszta77@gmail.com>
2025-11-17 19:55:13 +0100

    Navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,28 @@
+import { useGSAP } from "@gsap/react";
 import { navLinks } from "../../constants";
+import gsap from "gsap";
 
 export default function Navbar() {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "buttom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  }, []);
+
   return (
     <nav className="">
       <div>
