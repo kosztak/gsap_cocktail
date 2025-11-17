c48c0bf932a7cb4a62589613e9a5671caa4aceae
Ákos <kokoszta77@gmail.com>
2025-11-17 19:43:26 +0100

    Public files moved to root folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+    </>
   );
 }
