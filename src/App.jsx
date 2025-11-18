6f9d7d693f8fee96b8e7113e986559208d630956
Ákos <kokoszta77@gmail.com>
2025-11-18 12:47:20 +0100

    Hero banner implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,7 +12,10 @@ export default function App() {
       <header>
         <Navbar />
       </header>
-      <main></main>
+      <main>
+        <Hero />
+        <Cocktails />
+      </main>
     </>
   );
 }
